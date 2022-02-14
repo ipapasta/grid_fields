b3a87e4f49b35b3f69e3d7b75da1fe4319600a9d
Ioannis Papastathopoulos <i.papastathopoulos@ed.ac.uk>
2022-02-14 15:02:06 +0000

    renaming file for posterior summary statistics

---
+++ R/posterior_summary_statistics.R
@@ -0,0 +1,54 @@
+source("Functions.R")
+
+## 
+## 
+## 
+posterior.gridness.score.M0 <- posterior.spatial.gridness.score(fit.space)
+posterior.gridness.score.M1 <- posterior.spatial.gridness.score(fit.space.direction)
+posterior.spatial.stdev.M0          <- posterior.spatial.standard.deviation(fit.space)
+posterior.spatial.stdev.M1          <- posterior.spatial.standard.deviation(fit.space.direction)
+posterior.spatial.range.M0          <- posterior.spatial.range(fit.space)
+posterior.spatial.range.M1          <- posterior.spatial.range(fit.space.direction)
+## 
+posterior.range.M0          <- posterior.spatial.range(fit.space)
+posterior.range.M1          <- posterior.spatial.range(fit.space.direction)
+## 
+posterior.directional.stdev.M1          <- posterior.directional.standard.deviation(fit.space.direction)
+posterior.directional.range.M1          <- posterior.directional.range(fit.space.direction)
+
+
+## 
+## 
+## 
+par(mfrow=c(3,2))
+plot(posterior.gridness.score.M0, type="l", xlim=c(-1,1), ylim=c(0, 10))
+lines(seq(-.99, 1, len=1000), prior.phi_osc(seq(-.99, 1, len=1000), a=2, b=10, lg=FALSE), lty=2)
+plot(posterior.gridness.score.M1, type="l", xlim=c(-1,1), ylim=c(0, 10))
+lines(seq(-.99, 1, len=1000), prior.phi_osc(seq(-.99, 1, len=1000), a=2, b=10, lg=FALSE), lty=2)
+## 
+## par(mfrow=c(1,2))
+plot(posterior.spatial.stdev.M0, type="l", xlim=c(0,10), ylim=c(0, 2))
+lines(seq(0, 10, len=1000), dexp(seq(0, 10, len=1000), 1/2), lty=2)
+plot(posterior.spatial.stdev.M1, type="l", xlim=c(0,10), ylim=c(0, 2))
+lines(seq(0, 10, len=1000), dexp(seq(0, 10, len=1000), 1/2), lty=2)
+## 
+## par(mfrow=c(1,2))
+plot(posterior.spatial.range.M0, type="l", xlim=c(0,60), ylim=c(0, .61))
+lines(seq(0, 100, len=1000), dlnorm(seq(0, 100, len=1000), log(25), 3), lty=2)
+plot(posterior.spatial.range.M1, type="l", xlim=c(0,60), ylim=c(0, .61))
+lines(seq(0, 100, len=1000), dlnorm(seq(0, 100, len=1000), log(25), 3), lty=2)
+
+## directional parameters
+plot(posterior.gridness.score.M0, type="l", xlim=c(-1,1), ylim=c(0, 10))
+lines(seq(-.99, 1, len=1000), prior.phi_osc(seq(-.99, 1, len=1000), a=2, b=10, lg=FALSE), lty=2)
+plot(posterior.gridness.score.M1, type="l", xlim=c(-1,1), ylim=c(0, 10))
+lines(seq(-.99, 1, len=1000), prior.phi_osc(seq(-.99, 1, len=1000), a=2, b=10, lg=FALSE), lty=2)
+##
+
+par(mfrow=c(1,2))
+plot(posterior.directional.stdev.M1, type="l", xlim=c(0,10), ylim=c(0, 2))
+lines(seq(0, 10, len=1000), dexp(seq(0, 10, len=1000), sigma.directional), lty=2)
+## 
+## par(mfrow=c(1,2))
+plot(posterior.directional.range.M1, type="l", xlim=c(0,10), ylim=c(0, 4))
+lines(seq(0, 10, len=1000), dexp(seq(0, 10, len=1000), rho.directional), lty=2)
